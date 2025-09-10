913a2b95da3bc7ea57cc5f181501254fd90824d7
Victor Iak <victorciak2013@gmail.com>
2025-09-09 22:30:26 -0300

    Adição de aúdio a bola

---
+++ Assets/AudioResource.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioResource : MonoBehaviour
+{
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    void OnCollisionEnter2D (Collision2D coll) {
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
